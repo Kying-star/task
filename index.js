29bed61e6ed2ff88c373072cc20eb9cd631a10bc
Qixing Lin <3077006505@qq.com>
2020-08-01 10:57:36 +0800

    master

---
+++ index.js
@@ -0,0 +1,9 @@
+/*
+ * @Author: your name
+ * @Date: 2020-07-28 08:23:04
+ * @LastEditTime: 2020-08-01 10:56:57
+ * @LastEditors: 代码是写出来给人看的，附带能在机器上运行
+ * @Description: In User Settings Edit
+ * @FilePath: \git_task\index.js
+ */
+document.write("this is index.js");
