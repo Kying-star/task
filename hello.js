7a9b208543b1be07df5e53a3d15a2398add32259
Qixing Lin <3077006505@qq.com>
2020-08-01 11:05:49 +0800

    add hello

---
+++ hello.js
@@ -0,0 +1,9 @@
+/*
+ * @Description:
+ * @Version: 0.0
+ * @Autor: 林其星
+ * @Date: 2020-08-01 11:02:30
+ * @LastEditors: 代码是写出来给人看的，附带能在机器上运行
+ * @LastEditTime: 2020-08-01 11:02:57
+ */
+console.log("hello");
